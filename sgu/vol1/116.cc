55ef369c39af9d333d203fc4851b9121bde614b5
veecci <veecci@gmail.com>
2013-05-06 09:15:23 +0800

    number_theory, dp

--- sgu/vol1/116.cc
+++ sgu/vol1/116.cc
@@ -20,7 +20,7 @@ int pri[maxn], mark[maxn], cnt;
 void sieve() {
   cnt = 0, mark[0] = mark[1] = 1;
   for (int i = 2; i < maxn; ++i) {
-  	if (!mark[i]) pri[cnt++] = mark[i] = i;
+		if (!mark[i]) pri[cnt++] = mark[i] = i;
 		for (int j = 0; pri[j] * i < maxn; ++j) {
 			mark[i * pri[j]] = pri[j];
 			if (!(i % pri[j])) break;
