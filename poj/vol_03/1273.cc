2ea8edf407455b2b635a94d450f20ed8fd9c8f34
veecci <veecci@gmail.com>
2013-10-31 02:08:57 +0800

    graph_theory, max_flow

--- poj/vol_03/1273.cc
+++ poj/vol_03/1273.cc
@@ -2,75 +2,68 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#define MAXN 205
-#define MAXM 410
-#define inf 0x7f7f7f7f
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <ctime>
+
+#define inf 0x3f3f3f3f
+#define Inf 0x3FFFFFFFFFFFFFFFLL
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i, n) for (int i = 1; i <= (n); ++i)
+#define clr(x, a) memset(x, (a), sizeof x)
 using namespace std;
-struct edges{
-    int u,c,next;
-}e[MAXM];
-int p[MAXN],idx;
-int N,M;
-void addedge(int u,int v,int c,int cc=0){
-  //  printf("e[%d]:(%d,%d) = %d\n",idx,u,v,c);
-    e[idx].u=v; e[idx].c=c;  e[idx].next=p[u]; p[u]=idx++;
-  //  printf("e[%d]:(%d,%d) = %d\n",idx,v,u,c);
-    e[idx].u=u; e[idx].c=cc; e[idx].next=p[v]; p[v]=idx++;
-
+typedef long long ll;
+int const N = 210;
+int const M = 410;
+int n, m;
+struct edges{ int u, w, next; } e[M];
+int p[N], idx;
+void addedge(int u, int v, int c, int r = 0) {
+	e[idx].u = v, e[idx].w = c, e[idx].next = p[u], p[u] = idx++;
+	e[idx].u = u, e[idx].w = r, e[idx].next = p[v], p[v] = idx++;
 }
-void init(){ idx=0; memset(p,0xff,sizeof(p));}
-
-int gap[MAXN],dis[MAXN],pre[MAXN],cur[MAXN];
-
-int sap(int s,int t){
-    memset(dis,0,sizeof(dis));
-    memset(gap,0,sizeof(gap));
-    for(int i=1;i<=N;i++)cur[i]=p[i];
-    int u=pre[s]=s, max_flow=0,step=inf;
-    gap[0]=N;
-    while(dis[s]<N){
-loop:   for(int &i=cur[u];i!=-1;i=e[i].next){
-            int v=e[i].u;
-            if(e[i].c>0 && dis[u]==dis[v]+1){
-                step=min(step,e[i].c);
-                pre[v]=u;
-                u=v;
-                if(v==t){
-                    max_flow += step;
-                    for(u=pre[u];v!=s;v=u,u=pre[u]){
-                        e[cur[u]].c -= step;
-                        e[cur[u]^1].c += step;
-                    }
-                    step=inf;
-                }
-                goto loop;
-            }
-        }
-        int mindis=N;
-        for(int i=p[u];i!=-1;i=e[i].next){
-            int v=e[i].u;
-            if(e[i].c>0 && mindis>dis[v]){
-                cur[u]=i;
-                mindis=dis[v];
-            }
-        }
-        if( (--gap[dis[u]])==0) break;
-        gap[ dis[u] = mindis+1] ++;
-        u=pre[u];
-    }
-    return max_flow;
+void init() { idx = 0; clr(p, 0xff); }
+bool vis[N]; 
+int dfs(int u, int t, int f) {
+	if (u == t) return f;
+	vis[u] = 1;
+	for (int i = p[u]; ~i; i = e[i].next) {
+		int v = e[i].u, c = min(f, e[i].w);;
+		if (!vis[v] && c > 0) {
+			int r = dfs(v, t, c);
+			if (r > 0) {
+				e[i].w -= r, e[i ^ 1].w += r;
+				return r;
+			}
+		}
+	}
+	return 0;
+}
+int cal(int s, int t) {
+	int ret = 0, c = 0;
+	do {
+		clr(vis, 0);
+		c = dfs(s, t, inf);
+		ret += c;
+	} while (c > 0);
+	return ret;
+}
+int main() {
+	int a, b, c;
+	while (~scanf("%d%d", &m, &n)) {
+		init();
+		rep(i, m) {
+			scanf("%d%d%d", &a, &b, &c);
+			addedge(a, b, c);
+		}
+		printf("%d\n", cal(1, n));
+	}
+	return 0;
 }
 
-int main(){
-    int a,b,c;
-    //freopen("poj_1273.txt","r",stdin);
-    while(~scanf("%d%d",&M,&N)){
-        init();
-        for(int i=0;i<M;i++){
-            scanf("%d %d %d",&a,&b,&c);
-            addedge(a,b,c);
-        }
-        printf("%d\n",sap(1,N));
-    }
-    return 0;
-}
