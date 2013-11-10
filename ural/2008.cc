5f5dcd7e0a956ce25a9fc995d0f50deb8cf70c4e
veecci <veecci@gmail.com>
2013-11-10 11:29:02 +0800

    Update 2008.cc

--- ural/2008.cc
+++ ural/2008.cc
@@ -19,20 +19,19 @@
 #define clr(x, a) memset(x, (a), sizeof x)
 using namespace std;
 typedef long long ll;
-typedef double db;
+typedef long double db;
 db const g = 9.8;
-db const eps = 1e-11;
+db const eps = 1e-6;
 db const hpi = 0.5 * acos(-1.0);
 int sgn(db x) { return x < -eps ? -1 : x < eps ? 0 : 1; }
-
 bool check(db v, db x1, db y1, db x2, db y2) {
 	if (x1 > x2) { swap(x1, x2); }
-	for (db d = 0.0; d <= hpi; d += 0.000005) {
+	for (db d = 0.0; d <= hpi; d += 0.000002) {
 		db s = y2 - y1;
 		db vx = v * cos(d), vy = v * sin(d);
 		db dt = vy * vy - 2.0 * g * s;
 		if (sgn(dt) < 0) continue;
-		dt += eps;
+		if (dt < 0.0) dt = 0.0;
 		db t = (vy + sqrt(dt)) / g;
 		if (sgn(vx * t - (x2 - x1)) >= 0) return 1;
 	}
